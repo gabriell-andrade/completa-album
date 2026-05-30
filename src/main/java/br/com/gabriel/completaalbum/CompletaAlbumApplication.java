3d097a193d0610e10490a2729b36aa9b8c23e3cb
Gabriel Andrade <gabriel_anme@hotmail.com>
2026-05-30 16:59:56 -0300

    first commit

---
+++ src/main/java/br/com/gabriel/completaalbum/CompletaAlbumApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.completaalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletaAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletaAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/completaalbum/CompletaAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.completaalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletaAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
