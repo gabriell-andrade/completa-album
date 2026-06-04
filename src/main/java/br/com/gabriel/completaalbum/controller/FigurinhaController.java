f6206e4d5f2ea170a2b0a3a199aa700af26189e9
Gabriel Andrade <gabriel_anme@hotmail.com>
2026-06-04 01:30:31 -0300

    feat: configura documentacao OpenAPI

--- src/main/java/br/com/gabriel/completaalbum/controller/FigurinhaController.java
+++ src/main/java/br/com/gabriel/completaalbum/controller/FigurinhaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import br.com.gabriel.completaalbum.dto.ProgressoResponse;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
@@ -19,21 +20,33 @@ public class FigurinhaController {
         this.service = service;
     }
 
+    @Operation(
+            summary = "Lista todas as figurinhas do álbum"
+    )
     @GetMapping("/figurinhas")
     public List<Figurinha> listarTodas() {
         return service.listarTodas();
     }
 
+    @Operation(
+            summary = "Lista as figurinhas obtidas do álbum"
+    )
     @GetMapping("/figurinhas/obtidas")
     public List<Figurinha> listarObtidas() {
         return service.listarObtidas();
     }
 
+    @Operation(
+            summary = "Lista as figurinhas faltantes do álbum"
+    )
     @GetMapping("/figurinhas/faltantes")
     public List<Figurinha> listarFaltantes() {
         return service.listarFaltantes();
     }
 
+    @Operation(
+            summary = "Alterna o status da figurinha"
+    )
     @PatchMapping("/figurinhas/{codigo}")
     public Figurinha alternarObtida(
             @PathVariable String codigo
@@ -41,11 +54,17 @@ public class FigurinhaController {
         return service.alternarObtida(codigo);
     }
 
+    @Operation(
+            summary = "Mostra o progresso do álbum"
+    )
     @GetMapping("/figurinhas/progresso")
     public ProgressoResponse obterProgresso() {
         return service.obterProgresso();
     }
 
+    @Operation(
+            summary = "Busca a figurinha do álbum por código"
+    )
     @GetMapping("/figurinhas/{codigo}")
     public Figurinha buscarPorCodigo(
             @PathVariable String codigo
@@ -53,10 +72,16 @@ public class FigurinhaController {
         return service.buscarPorCodigo(codigo);
     }
 
+    @Operation(
+            summary = "Lista as figurinhas faltantes do álbum por seção"
+    )
     @GetMapping("/figurinhas/secao/{secao}")
     public List<Figurinha> listarPorSecao(
             @PathVariable String secao
     ) {
         return service.listarPorSecao(secao);
     }
+
+
+
 }
